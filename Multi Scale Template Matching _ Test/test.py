2a6892d47fcf89bb7a29d1fb54777661a7329039
AdarshKuzhipathalil <33592675+AdarshKuzhipathalil@users.noreply.github.com>
2019-05-20 23:55:31 +0200

    Update test.py

--- Multi Scale Template Matching _ Test/test.py
+++ Multi Scale Template Matching _ Test/test.py
@@ -12,43 +12,6 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 
-'''
-# Read the main image 
-img_rgb = cv2.imread('main1.jpg') 
-
-# Convert it to grayscale 
-
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
-
-
-# Read the template 
-template = cv2.imread('template1.jpg',0) 
-#template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-
-
-# Store width and heigth of template in w and h 
-w, h = template.shape[::-1] 
-
-# Perform match operations. 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED) 
-print(res)
-# Specify a threshold 
-threshold = 0.7
-
-# Store the coordinates of matched area in a numpy array 
-loc = np.where( res >= threshold)
-print(loc)
- 
-
-# Draw a rectangle around the matched region. 
-for pt in zip(*loc[::-1]): 
-	cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2) 
-
-# Show the final image with the matched area. 
-
-cv2.imshow('Detected',img_rgb) 
-cv2.waitKey()
-'''
 
 def template_scaling(image_path,des):
  
